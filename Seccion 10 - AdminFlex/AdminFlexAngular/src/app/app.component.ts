85a9e6ea369c2b52af15aab98a76079a677165cb
Alfredo Garcia <agarcia3110@gmail.com>
2021-01-10 00:19:04 +0100

    AdminFlex Completado

--- Seccion 10 - AdminFlex/AdminFlexAngular/src/app/app.component.ts
+++ Seccion 10 - AdminFlex/AdminFlexAngular/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AdminFlexAngular';
+    title = 'AdminFlexAngular';
+    claseFecha: string = 'fas fa-arrow-left';
+    sidebarDesplegado: boolean = true;
+
+    desplegarOcultarSidebar() {
+        this.sidebarDesplegado = !this.sidebarDesplegado;
+    }
 }
--- Seccion 10 - AdminFlex/AdminFlexAngular/src/assets/js/app.js
+++ Seccion 10 - AdminFlex/AdminFlexAngular/src/assets/js/app.js
@@ -1,22 +1,24 @@
 const menuIzquierdo = document.querySelector('.menu-izquierdo');
 
-menuIzquierdo.addEventListener('click', (e)  => {
-       const claseMenu = e.target.classList;
-       
-       // Selecciona el contenedor
-       const contenedor = document.querySelector('.pagina'),
-             flechaIzq = document.querySelector('.fa-arrow-left'),
-             flechaDer = document.querySelector('.fa-arrow-right');
+menuIzquierdo.addEventListener('click', (e) => {
 
-       if(claseMenu.contains('fa-arrow-left') ) {
-            // cerrar el menú lateral
-            contenedor.classList.add('no-menu');
-            e.target.style.display = 'none';
-            flechaDer.style.display = 'block';
-       } else if( claseMenu.contains('fa-arrow-right')) {
-            contenedor.classList.remove('no-menu');
-            e.target.style.display = 'none';
-            flechaIzq.style.display = 'block';
-       }
+    console.log(e);
+    //    const claseMenu = e.target.classList;
+
+    //    // Selecciona el contenedor
+    //    const contenedor = document.querySelector('.pagina'),
+    //          flechaIzq = document.querySelector('.fa-arrow-left'),
+    //          flechaDer = document.querySelector('.fa-arrow-right');
+
+    //    if(claseMenu.contains('fa-arrow-left') ) {
+    //         // cerrar el menú lateral
+    //         contenedor.classList.add('no-menu');
+    //         e.target.style.display = 'none';
+    //         flechaDer.style.display = 'block';
+    //    } else if( claseMenu.contains('fa-arrow-right')) {
+    //         contenedor.classList.remove('no-menu');
+    //         e.target.style.display = 'none';
+    //         flechaIzq.style.display = 'block';
+    //    }
 });
 
